c76f0e2ea8a26f7f6d0cd70e45c5262f719cfbd9
NicoFilips <git@nicofilips.ch>
2024-09-25 20:39:33 +0200

    feat(program.cs): add intro

--- Solid-showcase/Program.cs
+++ Solid-showcase/Program.cs
@@ -1,16 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// OPEN CLOSED PRINCIPLE 
-
-using Solid_showcase.Solid._2_Open_Closed;
-using Calculator = Solid_showcase.Solid._2_Open_Closed.BadExample.Calculator;
-
-IOperation addition = new Addition();
-Calculator calculator = new Calculator(addition);
-int result = calculator.Calculate(5, 3);
-Console.WriteLine($"Addition: {result}");
-
-IOperation multiplication = new Multiplication();
-calculator = new Calculator(multiplication);
-result = calculator.Calculate(5, 3);
-Console.WriteLine($"Multiplikation: {result}");
+Console.WriteLine("Welcome to my little SOLID Showcase!");
